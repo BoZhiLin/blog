a47be73c9108b6df377c0520e40c569f45791fe7
BoZhiLin <s1410432004@gms.nutc.edu.tw>
2018-07-19 23:05:45 +0800

    fix element tag

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<form action="{{ route('post.store') }}" method="POST">
-    @csrf
-    標題：<input type="text" name="title">
-    內容：<textarea name="content"></textarea>
-    <input type="submit" class="btn btn-primary" value="送出">
-<form>
+    <form action="{{ route('post.store') }}" method="POST">
+        @csrf
+        標題：<input type="text" name="title">
+        內容：<textarea name="content"></textarea>
+        <input type="submit" class="btn btn-primary" value="送出">
+    </form>
 @endsection
