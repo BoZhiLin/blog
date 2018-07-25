985e0b618f04a4f156b7c4ae265078eca4e84c24
BoZhiLin <s1410432004@gms.nutc.edu.tw>
2018-07-25 22:02:11 +0800

    modify route sort

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,11 @@
 |
 */
 
+Auth::routes();
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
-
 Route::resource('post', 'PostController');
